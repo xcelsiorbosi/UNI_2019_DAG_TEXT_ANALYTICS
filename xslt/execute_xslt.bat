66d457e8794c26b4706fa8c1bfd616e200956789
DiveIT <noakat@hotmail.com>
2019-10-10 15:55:15 +1030

    Deleted unneeded data files.

--- xslt/execute_xslt.bat
+++ xslt/execute_xslt.bat
@@ -3,7 +3,7 @@ rem Question Time
 java -jar c:/saxon/saxon9he.jar input/HANSARD-10-27650.xml HANSARD_General.xsl -o:output/xslt_HANSARD-10-27650_question_time.xml
 
 rem Answers to Questions
-java -jar c:/saxon/saxon9he.jar ../data/HANSARD-11-32902.xml HANSARD_General.xsl -o:output/xslt_HANSARD-11-32902_questions_and_answers.xml
+java -jar c:/saxon/saxon9he.jar input/HANSARD-11-32902.xml HANSARD_General.xsl -o:output/xslt_HANSARD-11-32902_questions_and_answers.xml
 
 rem Bills
 java -jar c:/saxon/saxon9he.jar input/HANSARD-10-26906.xml HANSARD_General.xsl -o:output/xslt_HANSARD-10-26906_bill.xml
