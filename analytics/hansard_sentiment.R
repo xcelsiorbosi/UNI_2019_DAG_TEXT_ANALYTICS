647e1245b1fff719ce97f6da14208723b1f53807
DiveIT <noakat@hotmail.com>
2019-10-10 15:58:13 +1030

    Sentiment is now calculated for each TextID as well as each HansardID. Added sentiment to heat maps.

--- analytics/hansard_sentiment.R
+++ analytics/hansard_sentiment.R
@@ -5,48 +5,73 @@ library(dplyr)
 library(syuzhet)
 library(lubridate)
 library(sentimentr)
+library(ggplot2)
+
+options(scipen = 999) # turn off scientific notation
 
 setwd("D:/Education/2015-2019 - Master of Data Science/2019-S2 - Capstone Professional Project/Project Code")
 
 # Import Text sheet
-data = read_xlsx("./data/Hansard1102019.xlsx", sheet = "Text")
+data = read_xlsx("./data/HansardText10102019.xlsx", col_types = "text")
 head(data)
+head(data[data$HansardID == "HANSARD-11-27883.xml",]) # Check TextID is no longer using scientific notation
 
-# Import HANSARDFilesInfo sheet
-#HANSARDFilesInfo = data.frame(read_xlsx("./data/Hansard1102019.xlsx", sheet = "HANSARDFilesInfo"))
-#head(HANSARDFilesInfo)
+# Get Sentiment for each Hansard ID
+   
+  # Combine text transcripts by HansardID
+  hansard_text = data %>% 
+    group_by(HansardID) %>% 
+    summarise(discussion = paste(Text, collapse = ". "))
+  head(hansard_text)
+  
+  # Get raw character vector
+  breaked_discussion = get_sentences(hansard_text$discussion)
+  
+  # Get sentiment for each Hansard Record
+  sentiment_discussion = sentiment_by(breaked_discussion)
+  head(sentiment_discussion)
+  
+  # Export sentiment score as CSV
+  hansard_text$sentiment_score = sentiment_discussion$ave_sentiment
+  hansard_text$sentiment_score_sd = sentiment_discussion$sd
+  hansard_text$discussion <- NULL
+  names(hansard_text) <- c("FileName", "SentimentScore", "SentimentScoreSD")
+  write.csv(hansard_text, "./analytics/HansardRecordSentiment.csv", row.names = FALSE)
+  
+  qplot(sentiment_discussion$ave_sentiment, geom="histogram",binwidth=0.05,
+        main="Hansard Record Sentiment",xlab="Sentiment",ylab="Number of Hansard Records")
+  summary(sentiment_discussion$ave_sentiment)
 
-# Combine text transcripts by HansardID
-hansard_text = data %>% 
-  #group_by(Kind,TalkerID,HansardID) %>% 
-  group_by(HansardID) %>% 
-  summarise(discussion = paste(Text, collapse = ". "))
-head(hansard_text)
+# Get Sentiment for each Text ID
 
-# Merge talker information and file information
-#hansard_text = data.frame(merge(x=hansard_text, y=HANSARDFilesInfo[,-c(1,4)], 
-#                                     by.x = "HansardID", by.y = "FileName"))
-#hansard_text$URL <- NULL
-#str(hansard_text)
+  hansard_text = data %>% 
+    group_by(TextID, HansardID) %>% 
+    summarise(discussion = paste(Text, collapse = ". "))
+  head(hansard_text)
+  
+  # Get raw character vector
+  breaked_discussion = get_sentences(hansard_text$discussion)
+  
+  # Get sentiment for each Hansard Record
+  sentiment_discussion = sentiment_by(breaked_discussion)
+  head(sentiment_discussion)
+  
+  # Export sentiment score as CSV
+  hansard_text$sentiment_score = sentiment_discussion$ave_sentiment
+  hansard_text$sentiment_score_sd = sentiment_discussion$sd
+  hansard_text$discussion <- NULL
+  names(hansard_text) <- c("TextID","FileName", "SentimentScore", "SentimentScoreSD")
+  head(hansard_text)
+  write.csv(hansard_text, "./analytics/HansardTextSentiment.csv", row.names = FALSE)
+  
+  qplot(sentiment_discussion$ave_sentiment, geom="histogram",binwidth=0.05,
+        main="Hansard Text Sentiment",xlab="Sentiment",ylab="Number of Text Fragments")
+  summary(sentiment_discussion$ave_sentiment)
 
-rm(data) # remove unneeded data
-
-# Get raw character vector
-breaked_discussion = get_sentences(hansard_text$discussion)
-
-# Get sentiment for each Hansard Record
-sentiment_discussion = sentiment_by(breaked_discussion)
-head(sentiment_discussion)
-
-# Export sentiment score as CSV
-hansard_text$sentiment_score = sentiment_discussion$ave_sentiment
-hansard_text$discussion <- NULL
-names(hansard_text) <- c("FileName", "SentimentScore")
-write.csv(hansard_text, "./analytics/HansardRecordSentiment.csv", row.names = FALSE)
-
-rm(breaked_discussion, hansard_text, sentiment_discussion)
+rm(data, breaked_discussion, hansard_text, sentiment_discussion)
 
 # https://www.tidytextmining.com/sentiment.html
 # https://towardsdatascience.com/sentiment-analysis-in-r-good-vs-not-good-handling-negations-2404ec9ff2ae
 # https://www.datacamp.com/community/tutorials/sentiment-analysis-R
 # https://www.kaggle.com/rtatman/tutorial-sentiment-analysis-in-r
+# https://medium.com/@mattifuchs/doing-your-first-sentiment-analysis-in-r-with-sentimentr-167855445132
