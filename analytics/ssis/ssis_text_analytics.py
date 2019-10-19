0ea65f183e31afa4b0c2eb5ba5a876d105e8b2ea
DiveIT <noakat@hotmail.com>
2019-10-20 09:48:48 +1030

    Full record text is now stored in HANSARD database. Updated dashboards to get record text from database rather than Excel spreadsheet. Deleted unneeded data files. Updated Tableau data extract.

--- analytics/ssis/ssis_text_analytics.py
+++ analytics/ssis/ssis_text_analytics.py
@@ -5,7 +5,8 @@ from text_summary_statistics import word_count, get_keywords
 from document_summary import smart_truncate, generate_summary_from_text
 
 # Connect to HANSARD database
-db_connection = pyodbc.connect('Driver={SQL Server};'
+#print(pyodbc.drivers()) # Print available ODBC drivers
+db_connection = pyodbc.connect('Driver={SQL Server Native Client 11.0};'
                       'Server=DA-PROD1;'
                       'Database=HANSARD;'
                       'Trusted_Connection=yes;')
@@ -13,9 +14,9 @@ db_connection = pyodbc.connect('Driver={SQL Server};'
 db_cursor = db_connection.cursor()
 
 # Get HANSARDFilesInfo table
-info = pd.read_sql_query("SELECT ID, KeyWords, Summary, TruncatedSummary FROM HANSARD.dbo.HANSARDFilesInfo", 
+info = pd.read_sql_query("SELECT ID, KeyWords, Summary, TruncatedSummary, RecordText FROM HANSARD.dbo.HANSARDFilesInfo", 
                          db_connection)
-info = info.astype({"ID":'str', "KeyWords":'str', "Summary":'str', "TruncatedSummary":'str'}) 
+info = info.astype({"ID":'str', "KeyWords":'str', "Summary":'str', "TruncatedSummary":'str', "RecordText":'str'}) 
 
 # Get FinalText table
 text = pd.read_sql_query("SELECT *  FROM HANSARD.dbo.FinalText", db_connection)
@@ -28,7 +29,6 @@ def word_count_db(hansard_id, text_id, text):
     if text is not None:
         text_word_count = word_count(text)
     
-    print(hansard_id, text_id, text_word_count, sep=" -- ") # DELETE WHEN FINISH TESTING
     db_cursor.execute("UPDATE HANSARD.dbo.FinalText SET WordCount = ? WHERE TextID = ? AND HansardID = ?", 
                       text_word_count, text_id, hansard_id)
 
@@ -54,22 +54,26 @@ for index, row in combined.iterrows():
     # Calculate and add keywords to HANSARDFilesInfo table if it does not exist
     if row['KeyWords'] == 'None':
         key_words = get_keywords(full_text)
-        print(hansard_id, key_words, sep=" -- ") # DELETE WHEN FINISH TESTING
         db_cursor.execute("UPDATE HANSARD.dbo.HANSARDFilesInfo SET KeyWords = ? WHERE ID = ?", 
                           key_words, hansard_id)
 
     # Document Summaries
     if row['Summary'] == 'None':
         summary = generate_summary_from_text(full_text, 3, False)
-        print(hansard_id, summary, sep=" -- ") # DELETE WHEN FINISH TESTING
         db_cursor.execute("UPDATE HANSARD.dbo.HANSARDFilesInfo SET Summary = ? WHERE ID = ?", 
                           summary, hansard_id)
 
     if row['TruncatedSummary'] == 'None':
         summary = smart_truncate(full_text)
-        print(hansard_id, summary, sep=" -- ") # DELETE WHEN FINISH TESTING
         db_cursor.execute("UPDATE HANSARD.dbo.HANSARDFilesInfo SET TruncatedSummary = ? WHERE ID = ?", 
                           summary, hansard_id)
+
+    # Full Record Text
+    if row['RecordText'] == 'None':
+        print(hansard_id, "Full Text", sep=" -- ") # DELETE WHEN FINISH TESTING
+        db_cursor.execute("UPDATE HANSARD.dbo.HANSARDFilesInfo SET RecordText = ? WHERE ID = ?", 
+                          full_text, hansard_id)
+
 
 # Determine whether text contains mentions of Audit Team Key Terms and add as new table to 
 # HANSARD database. Everytime this term search is run will replace the table in the database in case
@@ -128,7 +132,7 @@ it_terms['AuditTeam'] = "IT"
 # Merge search results
 merged_data = pd.concat([performance_terms, government_terms,it_terms], ignore_index=True)
 merged_data = merged_data.drop_duplicates() # Drop duplicate rows
-print(merged_data.shape)
+#print(merged_data.shape)
 
 # Add key term search results to KeyTerms table in HANSARD database
 db_cursor.execute("DELETE FROM HANSARD.dbo.KeyTerms") # Delete all rows from table
