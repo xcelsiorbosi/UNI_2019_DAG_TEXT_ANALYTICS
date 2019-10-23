c8d0f6668f223e01b2c1ed8837c8272239becb06
DiveIT <noakat@hotmail.com>
2019-10-23 21:33:53 +1030

    Applied naming conventions

--- analytics/ssis/AGDclients.R
+++ analytics/ssis/AGDclients.R
@@ -1,4 +1,4 @@
-#loading requred library
+# Load required libraries
 library(readxl)
 library(tidyr)
 library(dplyr)
@@ -6,46 +6,39 @@ library(stringr)
 library(RODBC)
 
 
-#importing Clients table
+# Import Clients and their alternate names from spreadsheet
 clients = read_xlsx("C:\\Users\\student2\\Documents\\GitHub\\UNI_2019_DAG_TEXT_ANALYTICS\\data\\Clients12102019.xlsx", sheet = "Sheet1")
-#Connecting to the Text table on the SQL Server DB
 
-dbhandle <- odbcDriverConnect('driver={SQL Server};server=DA-PROD1;database=HANSARD;trusted_connection=true')
-currTableSQL<-paste("SELECT *  FROM HANSARD.dbo.FinalText",sep="")
+# Connect to the Text table on the HANSARD SQL Server database
+db_connection <- odbcDriverConnect('driver={SQL Server};server=DA-PROD1;database=HANSARD;trusted_connection=true')
+text <- sqlQuery(db_connection, "SELECT *  FROM HANSARD.dbo.FinalText")
 
-Hansard1102019 <-sqlQuery(dbhandle,currTableSQL)
+client_list <- c(clients$AGDClient)
 
-
-clientlist <- c(clients$AGDClient)
-
-#here header is the dataframe where we need to search for client name
-# discussion is the texts from which column cleint name is to be searched
-
-dataframe_list <- lapply(clientlist, function(clientName) {
-  Hansard1102019 %>% filter(str_detect(Text, regex(clientName, ignore_case = T))) 
+# Text is the record from which column client name is to be searched
+search_results <- lapply(client_list, function(client_name) {
+  text %>% filter(str_detect(Text, regex(client_name, ignore_case = T))) 
 })
 
-#assigning client name to the dataframe
-names(dataframe_list) <- clientlist
+# Assign client name to the dataframe
+names(search_results) <- client_list
 
-#flat file with client name
+# Create flat file with client name
 library(plyr)
-Client_File <- ldply(dataframe_list, data.frame)
+search_results <- ldply(search_results, data.frame)
 detach(package:plyr)
 
-#Changing the column name
-names(Client_File)[1]<-"AGDClient"
+# Change the column name
+names(search_results)[1]<-"AGDClient"
 
-# Merging the client_File and clients to get the Formal Clients and the Client Types
-TextClient1 <- merge(x = Client_File, y = clients, by = "AGDClient", all.x = TRUE)
+# Merge the client_File and clients to get the Formal Clients and the Client Types
+search_results <- merge(x = search_results, y = clients, by = "AGDClient", all.x = TRUE)
 
-# Dropping the unnecessary columns and Rownames and renaming the HAnsardID
+# Drop the unnecessary columns and Rownames and renaming the HAnsardID
+text_client <- select(search_results, -c("TalkerID", "Text", "Kind", "WordCount"))
 
-TextClient <- select(TextClient1, -c("TalkerID", "Text", "Kind","WordCount"))
+# Insert the Data into ClientsMention table in HANSARD database
+sqlSave(db_connection, text_client[c("HansardID","TextID","AGDClient","AGDFormal","ClientType")], tablename = "ClientsMention", rownames = FALSE)
 
-
-# Inserting the Data into clientsmention table on Hansard DB on SS
-sqlSave(dbhandle, TextClient[c("HansardID","TextID","AGDClient","AGDFormal","ClientType")], tablename = "ClientsMention",rownames = FALSE)
-
-#Closing the DB Connection
-odbcClose(dbhandle)
+# Close the DB Connection
+odbcClose(db_connection)
